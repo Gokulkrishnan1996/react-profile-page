eabde7ebb389b7e45fb7f04579cfe6acec17f431
Gokulkrishnan <gokulkrishnan@ongil.io>
2022-10-01 20:42:45 +0530

    port update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const middleware = jsonServer.defaults({
   static: "./build",
 });
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 server.use(middleware);
 server.use(
   jsonServer.rewriter({
